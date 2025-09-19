caad68420ce33f1fb137e02e1279e6814a6842a3
Michael <michael.lima@agxsoftware.com>
2025-09-19 09:03:27 -0300

    aula 31 min 3

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -1,29 +1,55 @@
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import Loading from '../../components/layout/Loading/Loading'
+import Container from '../../components/layout/Container'
 
 function Project() {
   const { id } = useParams()
   const [project, setProject] = useState<any>(null)
 
+  // useEffect(() => {
+  //   fetch(`http://localhost:5000/projects/${id}`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //   })
+  //     .then((resp) => resp.json())
+  //     .then((data) => {
+  //       console.log(data)
+  //       setProject(data)
+  //     })
+  //     .catch((err) => console.log(err))
+  // }, [id])
+
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data)
-        setProject(data)
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
       })
-      .catch((err) => console.log(err))
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data)
+          setProject(data)
+        })
+        .catch((err) => console.log(err))
+    }, 3000)
   }, [id])
 
-
-  return(
-    <div>{project?.name}</div>
+  return (
+    <>
+      {project?.name ? (
+        <div>
+          <Container customClass="column"></Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   )
 }
 
